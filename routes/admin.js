5dd307a137c7e2502a645feb4c5c1c766366bdee
Anishkumar <anishpravitha@gmail.com>
2023-06-08 09:34:37 +0530

    Initial

--- routes/admin.js
+++ routes/admin.js
@@ -10,8 +10,6 @@ const fs = require('fs');
 
 
 
-const govtHelpers = require('../helpers/govt-helpers')
-const imageHelpers = require('../helpers/image-helpers')
 
 const { response } = require('express');
 const collections = require('../config/collections');
@@ -21,89 +19,7 @@ router.get('/', function(req, res, next) {
   res.render('',{admin:true});
 });
 
-router.get('/add-govt', function(req, res){
-  res.render('admin/add-govt',{admin:true})
-});
-router.get('/add-sampleel', function(req, res){
-  res.render('admin/sampleel',{admin:true})
-});
-router.post('/add-govt', (req, res) => {
-  var collectname = collections.GOVT_COLLECTION;
-  govtHelpers.addItem(req.body,collectname,(id) => {
-      res.render('admin/add-govt',{admin:true});
-  });
-});
-router.get('/view-govt', (req, res) => {
-  var collectname = collections.GOVT_COLLECTION;
-    govtHelpers.getAllItems(collectname).then((govt) => {
-      res.render('admin/view-govt', {govt, admin:true});
-    })
-});
-router.get('/delete-govt/:id', (req, res) => {
-  let govtId = req.params.id
-  var collectname = collections.GOVT_COLLECTION;
-  govtHelpers.deleteItem(govtId,collectname).then((response) => {
-    res.redirect('/admin/view-govt')
-  })
-})
-router.get('/edit-govt/:id', async (req, res) => {
-  var collectname = collections.GOVT_COLLECTION;
-  let govtcoll = await govtHelpers.getItemDetails(req.params.id,collectname);
-  console.log(govtcoll);
-  var items = govtcoll.items;
-    var allitems = govtcoll.allitems;
-  res.render('admin/edit-govt', { items, allitems, admin:true })
-})
-router.post('/edit-govt/:id', async (req, res) => {
-  var collectname = collections.GOVT_COLLECTION;
-  await govtHelpers.updateItem(req.params.id,req.body,collectname).then(() => {
-    res.redirect('/admin/view-govt')
-  })
-})
 
-
-router.get('/add-files', function(req, res){
-  res.render('admin/add-files',{admin:true})
-});
-router.post('/add-files', (req, res) => {
-  console.log(req.files.Image)
-  var collectname = collections.IMAGE_COLLECTION;
-  imageHelpers.addItem(req.body,collectname,(id) => {
-    let image = req.files.Image
-    console.log(id)
-    image.mv('./public/file-images/'+id+'.pdf',(err,done)=>{
-      if(!err){
-        res.render('admin/add-files',{admin:true});
-      }else{
-        console.log(err)
-      }
-    })
-      
-  });
-});
-
-
-router.get('/view-files', (req, res) => {
-  var collectname = collections.IMAGE_COLLECTION;
-    imageHelpers.getAllItems(collectname).then((image) => {
-      res.render('admin/view-files', {image, admin:true});
-    })
-});
-router.get('/delete-files/:id', (req, res) => {
-  let imageId = req.params.id
-  var collectname = collections.IMAGE_COLLECTION;
-  imageHelpers.deleteItem(imageId,collectname).then((response) => {
-    res.redirect('/admin/view-files')
-  })
-})
-
-
-router.get('/edit-files/:id', function (req, res, next) {
-  let fileId = req.params.id
-  const file = './public/file-images/'+fileId+'.pdf';
-  res.download(file);
-  
-})
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,6 @@ var collection = require('../config/collections');
 
 const { render, response } = require('../app');
 const userHelpers = require('../helpers/user-helpers');
-const govtHelpers = require('../helpers/govt-helpers');
-const imageHelpers = require('../helpers/image-helpers');
 const elsHelpers = require('../helpers/els-helpers')
 var router = express.Router();
 
@@ -15,7 +13,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   let user = req.session.user;
   console.log(user);
-  res.render('user/homepage', { user });
+  res.render('user/add-els', { user });
 });
 
 router.get('/login', (req, res) => {
@@ -51,49 +49,6 @@ router.get('/logout', function (req, res) {
   req.session.destroy();
   res.redirect('/');
 })
-
-router.get('/message', function (req, res, next) {
-  let user = req.session.user;
-  res.render('user/message', { user });
-});
-
-router.get('/view-govtcoll', function (req, res, next) {
-  let user = req.session.user;
-  var collectname = collection.GOVT_COLLECTION;
-  govtHelpers.getAllItems(collectname).then((govt) => {
-    res.render('user/view-govtcoll', { govt, user });
-  })
-});
-router.get('/edit-govtcoll/:id', async (req, res) => {
-  let user = req.session.user;
-  var collectname = collection.GOVT_COLLECTION;
-  let govtcoll = await govtHelpers.getItemDetails(req.params.id, collectname);
-  console.log(govtcoll);
-  var items = govtcoll.items;
-  var allitems = govtcoll.allitems;
-  res.render('user/edit-govtcoll', { items, allitems, user })
-})
-
-router.get('/download-file', function (req, res, next) {
-  const file = `files/letterreplace.docx`;
-  res.download(file);
-})
-
-router.get('/view-file', function (req, res, next) {
-  let user = req.session.user;
-  var collectname = collection.IMAGE_COLLECTION;
-  imageHelpers.getAllItems(collectname).then((image) => {
-    res.render('user/view-file', { image, user });
-  })
-});
-
-
-router.get('/edit-file/:id', function (req, res, next) {
-  let fileId = req.params.id
-  const file = './public/file-images/' + fileId + '.pdf';
-  res.download(file);
-
-});
 
 router.get('/add-els', function (req, res, next) {
   let user = req.session.user;
